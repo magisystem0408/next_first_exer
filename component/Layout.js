3efab89506baf94ccd20c49e08b9950480565a19
magisystem0408 <masato.matsudo@gmail.com>
2021-07-27 00:35:26 +0900

    commit

--- component/Layout.js
+++ component/Layout.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Image from "next/image";
 
 export default function Layout({children,title ="HP by Nextjs"}){
     return(
--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,6 @@ export default function Home() {
   return (
         <Layout title="home">
             <p className="text-4xl">Welcome to next</p>
-
-
         </Layout>
   );
 }
